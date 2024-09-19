6fd7ba1cd52e75864ac9ea95c554bcaf42da9217
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 17:01:13 +0200

    Saved in TypeScript Playground at 2024-09-19T15:01:13.339Z

--- arcs/index.ts
+++ arcs/index.ts
@@ -1,3 +1,22 @@
 function setup() {
   createCanvas(400, 400);
+
+  angleMode(Degrees);
+
+  stroke("black");
+  strokeWeight(10);
+  fill("yellow");
+
+  circle(200, 200, 375);
+
+  fill("black");
+  circle(125, 125, 20);
+  circle(275, 125, 20);
+
+  noFill();
+  
+
+
+
+
 }
