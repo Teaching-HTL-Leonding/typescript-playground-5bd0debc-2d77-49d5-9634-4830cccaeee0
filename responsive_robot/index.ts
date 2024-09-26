00aa0558f30023fbc82f02d49ba4cb7ce7e5dded
Rainer Stropek <rainer@timecockpit.com>
2024-09-26 14:13:07 +0200

    Saved in TypeScript Playground at 2024-09-26T12:13:06.799Z

--- responsive_robot/index.ts
+++ responsive_robot/index.ts
@@ -7,17 +7,17 @@ function setup() {
   const headHeight = height * 0.50; // Head size as 50% of the canvas width
   const headX = width / 2 - headWidth / 2; // Center the head horizontally
   const headY = height / 2 - headHeight / 2; // Center the head vertically
-  const eyes1 = headX+headWidth*0.25;
-  const eyes2 = headY+headWidth*0.25;
-  const mouthheight = headHeight*0.10;
-  const mouthweight = headWidth*0.50;
-  const mouthX = headX + headWidth*0.25; 
-  const mouthY = headY + headHeight*0.70;
-  const liniestartX = width*0.50;
-  const liniestartY = headY-headHeight*0.25;
-  const lineendX = width*0.50;
-  const lineendY = width*0.25;
-  
+  const eyes1 = headX + headWidth * 0.25;
+  const eyes2 = headY + headWidth * 0.25;
+  const mouthheight = headHeight * 0.10;
+  const mouthweight = headWidth * 0.50;
+  const mouthX = headX + headWidth * 0.25;
+  const mouthY = headY + headHeight * 0.70;
+  const liniestartX = width * 0.50;
+  const liniestartY = headY - headHeight * 0.25;
+  const lineendX = width * 0.50;
+  const lineendY = width * 0.25;
+
 
   // Draw the robot's head
   fill("gray");
@@ -27,14 +27,14 @@ function setup() {
   // Eyes
   fill("black");
   circle(eyes1, eyes2, 30);
-  circle(eyes1+100, eyes2, 30);
+  circle(eyes1 + 100, eyes2, 30);
 
   //Mouth
   fill("white");
   rect(mouthX, mouthY, mouthweight, mouthheight);
 
   // Antenne
-  line(liniestartX, liniestartY, lineendX, lineendY );
+  line(liniestartX, liniestartY, lineendX, lineendY);
 
   circle(lineendX, liniestartY, 20);
 }
