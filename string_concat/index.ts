001b36651c3b1b7581169814d7670a356ff60bea
Rainer Stropek <rainer@timecockpit.com>
2024-09-28 11:26:17 +0200

    Saved in TypeScript Playground at 2024-09-28T09:26:17.402Z

--- string_concat/index.ts
+++ string_concat/index.ts
@@ -2,6 +2,7 @@ function setup() {
   createCanvas(600, 600);
   background("lightblue");
 }
+
 let lastX: number = 0;
 let lastY: number = 0;
 let Koordinaten: string = '0/0, ';
@@ -9,16 +10,16 @@ let Koordinaten: string = '0/0, ';
 function mouseClicked() {
   // alten kreis und alten text überschreiben
   background("lightblue");
+  stroke("lightblue");
   // variablen zuweisen
   lastX = Math.round(mouseX);
   lastY = Math.round(mouseY);
   // neuer kreis
   fill("aqua");
-  stroke("aqua");
-  circle(lastX, lastY, 30);
+  //stroke("aqua");
+  circle(lastX, lastY, 40);
   // text erweitern 
   Koordinaten = Koordinaten + `${lastX}/${lastY}, `;
   fill("black");
-  stroke("black");
   text(`${Koordinaten}`, 10, 10, 590, 600);
 }
