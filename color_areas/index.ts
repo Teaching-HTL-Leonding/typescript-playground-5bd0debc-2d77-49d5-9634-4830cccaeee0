b172ebdaddcf69bbc0dbefb2c420864ff46da26d
Rainer Stropek <rainer@timecockpit.com>
2024-10-04 16:04:18 +0200

    Saved in TypeScript Playground at 2024-10-04T14:04:18.219Z

--- color_areas/index.ts
+++ color_areas/index.ts
@@ -2,7 +2,7 @@ function setup() {
     createCanvas(400, 200);
     background("black");
     noStroke();
-    
+
     fill("yellow");
     rect(0, 0, width / 3);
 
@@ -15,10 +15,23 @@ function setup() {
 
 function mouseClicked() {
 
+    fill("white");
     // The message variable will recive the message to print
     let message: string;
 
     // Note the `ìf` statement here. It checks is the mouse click
     // was in the area (yellow). If it was, the message is "Yellow".
+    if (mouseX < width / 3) {
+        message = "Yellow"
+    } else if (mouseX < width / 3 * 2) {
+        message = "Red";
+        // Note the `else if` statement here. If the user did not 
+        // click in the yellow area, it checks if the user clicked 
+        // in  the second area (green).
+    }else { 
+        // If the click was not in the yellow and not in the green
+        // area, the click must have been in the area.
+        message = "Green";
+    }
 
 }
