561b8148dc772d2d6fd496ff73484c53629a4302
Rainer Stropek <rainer@timecockpit.com>
2024-10-04 16:42:21 +0200

    Saved in TypeScript Playground at 2024-10-04T14:42:21.076Z

---
+++ color_quadrants/index.ts
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    noStroke();
+
+    // <<< Add your code here
+}
+
+// <<< Add the function `moveClicked` with the required code here
