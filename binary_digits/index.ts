3115b0aea4b4f14439daa8590f7f8a2475856ac3
Rainer Stropek <rainer@timecockpit.com>
2024-10-11 12:31:16 +0200

    Saved in TypeScript Playground at 2024-10-11T10:31:15.914Z

--- binary_digits/index.ts
+++ binary_digits/index.ts
@@ -1,23 +1,24 @@
+  
 function setup() {
   createCanvas(400, 400);
   background("black");
   const num = Math.floor(random(0, 1_000_000));
-  const lastDigit = num % 10;
-  let numWithoutLastDigit = Math.floor(num / 10);
+  const lastDigit = num % 2;
+  let numWithoutLastDigit = Math.floor(num / 2);
 
-  const digit2 = numWithoutLastDigit % 10
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 10);
+  const digit2 = numWithoutLastDigit % 2
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
 
-  const digit3 = numWithoutLastDigit % 10;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 10);
+  const digit3 = numWithoutLastDigit % 2;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
 
-  const digit4 = numWithoutLastDigit % 10;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 10);
+  const digit4 = numWithoutLastDigit % 2;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
 
-  const digit5 = numWithoutLastDigit % 10;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 10);
+  const digit5 = numWithoutLastDigit % 2;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
 
-  const digit6 = numWithoutLastDigit % 10;
+  const digit6 = numWithoutLastDigit % 2;
   
 
 
