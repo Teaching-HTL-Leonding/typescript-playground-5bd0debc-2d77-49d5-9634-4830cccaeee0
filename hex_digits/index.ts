ac96aa5c2151e9e211ad31515a1b13f0c1c406e7
Rainer Stropek <rainer@timecockpit.com>
2024-10-12 11:14:16 +0200

    Saved in TypeScript Playground at 2024-10-12T09:14:15.665Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -22,29 +22,52 @@ function setup() {
   rect(200, height / 2, 30, 50);
   rect(300, height / 2, 30, 50);
 
-  if(lastDigit === 10){
+  if (lastDigit === 10) {
     text("A", 315, height / 2 + 25);
   }
-  else if(lastDigit === 11){
+  else if (lastDigit === 11) {
     text("B", 315, height / 2 + 25);
   }
-  else if(lastDigit === 12){
+  else if (lastDigit === 12) {
     text("C", 315, height / 2 + 25);
   }
-  else if(lastDigit === 13){
+  else if (lastDigit === 13) {
     text("D", 315, height / 2 + 25);
   }
-  else if(lastDigit === 14){
+  else if (lastDigit === 14) {
     text("E", 315, height / 2 + 25);
   }
-  else if(lastDigit === 15){
+  else if (lastDigit === 15) {
     text("F", 315, height / 2 + 25);
   }
-  
-  
-  text(`${lastDigit}`, 315, height / 2 + 25);
+  else {
+    text(`${lastDigit}`, 315, height / 2 + 25);
+  }
 
-  text(`${digit2}`, 215, height / 2 + 25);
+  if (digit2 === 10) {
+    text("A", 215, height / 2 + 25);
+  }
+  else if (digit2 === 11) {
+    text("B", 215, height / 2 + 25);
+  }
+  else if (digit2 === 12) {
+    text("C", 215, height / 2 + 25);
+  }
+  else if (digit2 === 13) {
+    text("D", 215, height / 2 + 25);
+  }
+  else if (digit2 === 14) {
+    text("E", 215, height / 2 + 25);
+  }
+  else if (digit2 === 15) {
+    text("F", 215, height / 2 + 25);
+  }
+  else {
+    text(`${digit2}`, 215, height / 2 + 25);
+  }
+
+
+  
 
   text(`${digit3}`, 115, height / 2 + 25);
 }
