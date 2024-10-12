0a136dff8c702f42a242c8db556462f703402c75
Rainer Stropek <rainer@timecockpit.com>
2024-10-12 12:01:26 +0200

    Saved in TypeScript Playground at 2024-10-12T10:01:26.476Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -1,4 +1,5 @@
 let num = 0;
+let posmouse number = 0;
 
 function setup() {
   num = Math.floor(random(0, 16));
@@ -11,8 +12,17 @@ function setup() {
   fill("yellow");
   text(num + ` in binary?`, width / 2, 50);
 
+  noFill();
+  stroke("yellow");
+  rect(50, height / 2 - 40, 50, 80);
+  rect(130, height / 2- 40, 50, 80);
+  rect(210, height / 2- 40, 50, 80);
+  rect(290, height / 2- 40, 50, 80);
 }
 
 function mouseClicked() {
+  if (50 < posmouse AND posmouse < 100){
+
+  }
 
 }
