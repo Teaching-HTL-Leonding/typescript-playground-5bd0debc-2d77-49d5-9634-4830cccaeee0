b56f34a876b631d34f7301d2165021e49600ca4b
Rainer Stropek <rainer@timecockpit.com>
2024-10-12 11:09:04 +0200

    Saved in TypeScript Playground at 2024-10-12T09:09:04.101Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -1,14 +1,15 @@
 function setup() {
   createCanvas(400, 400);
   background("black");
-  const num = Math.floor(random(0, 64));
-  const lastDigit = num % 2;
-  let numWithoutLastDigit = Math.floor(num / 2);
+  const num = Math.floor(random(0, 4096));
+  const lastDigit = num % 16;
+  let numWithoutLastDigit = Math.floor(num / 16);
 
-  const digit2 = numWithoutLastDigit % 2
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
+  const digit2 = numWithoutLastDigit % 16
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 16);
 
-  const digit3 = numWithoutLastDigit % 2;
+  const digit3 = numWithoutLastDigit % 16;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 16);
 
   textAlign(CENTER, CENTER);
   fill("yellow");
@@ -20,8 +21,30 @@ function setup() {
   rect(100, height / 2, 30, 50);
   rect(200, height / 2, 30, 50);
   rect(300, height / 2, 30, 50);
+
+  if(lastDigit === 10){
+    text("A", 315, height / 2 + 25);
+  }
+  else if(lastDigit === 11){
+    text("B", 315, height / 2 + 25);
+  }
+  else if(lastDigit === 12){
+    text("C", 315, height / 2 + 25);
+  }
+  else if(lastDigit === 13){
+    text("D", 315, height / 2 + 25);
+  }
+  else if(lastDigit === 14){
+    text("E", 315, height / 2 + 25);
+  }
+  else if(lastDigit === 15){
+    text("F", 315, height / 2 + 25);
+  }
+  
   
   text(`${lastDigit}`, 315, height / 2 + 25);
+
   text(`${digit2}`, 215, height / 2 + 25);
+
   text(`${digit3}`, 115, height / 2 + 25);
 }
