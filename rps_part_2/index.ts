e3b4848746347db59a4d3ffc7b8e90e20f0653fe
Rainer Stropek <rainer@timecockpit.com>
2024-10-23 09:42:41 +0200

    Saved in TypeScript Playground at 2024-10-23T07:42:40.821Z

--- rps_part_2/index.ts
+++ rps_part_2/index.ts
@@ -1,7 +1,3 @@
-// We declare constants to avoid repeating "magic numbers". 
-// These numbers are used for positioning text and icons on the canvas.
-// Note that we use all-caps for constants that represent 
-// configuration values to make them easy to recognize.
 const TEXT_LEFT = 30;
 const STONE_LEFT = 50;
 const PAPER_LEFT = 175;
@@ -9,7 +5,8 @@ const SCISSORS_LEFT = 300;
 const ICON_WIDTH = 100;
 const ICON_TOP = 75;
 const ICON_HEIGHT = 100;
-
+const HUMAN_SYMBOLS_TOP = 150;
+let computerv2 : number = 0
 function setup() {
     createCanvas(500, 490);
     background("black");
@@ -18,37 +15,30 @@ function setup() {
     textSize(30);
     text("Human:", TEXT_LEFT, 50);
 
-    // Display the icons for "stone", "paper", and "scissors".
+
     textSize(75);
-    const HUMAN_SYMBOLS_TOP = 150;
     text("🪨", STONE_LEFT, HUMAN_SYMBOLS_TOP);
     text("📃", PAPER_LEFT, HUMAN_SYMBOLS_TOP);
     text("✂️", SCISSORS_LEFT, HUMAN_SYMBOLS_TOP);
 }
-let selected: string = "";
-let computer: string = "";
 
 function mouseMoved() {
-    // This function highlights the icon currently being hovered over by the mouse.
-    // <<< Add code here to disable the hover effect once the user has made a choice
+
     noFill();
     strokeWeight(3);
 
-    // In the following lines of code, we repeatedly check if the mouse
-    // is vertically in the relevant area. Therefore, we calculate the boolean
-    // value once and use the variable later multiple times.
+
     const isInVertical = mouseY >= ICON_TOP && mouseY < ICON_TOP + ICON_HEIGHT;
 
-    // Check if the mouse is over the "stone" icon.
+    if(computerv2 === 0){
     if (isInVertical && mouseX >= STONE_LEFT && mouseX < STONE_LEFT + ICON_WIDTH) {
-        stroke("yellow"); // Highlight with yellow.
+        stroke("yellow"); 
     } else {
-        stroke("black"); // Remove highlight by drawing in black.
+        stroke("black"); 
     }
-    // Draw a rectangle around the "stone" icon.
     rect(STONE_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT);
 
-    // Same for paper
+ 
     if (isInVertical && mouseX >= PAPER_LEFT && mouseX < PAPER_LEFT + ICON_WIDTH) {
         stroke("yellow");
     } else {
@@ -56,63 +46,59 @@ function mouseMoved() {
     }
     rect(PAPER_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT);
 
-    // Same for scissors
+
     if (isInVertical && mouseX >= SCISSORS_LEFT && mouseX < SCISSORS_LEFT + ICON_WIDTH) {
         stroke("yellow");
     } else {
         stroke("black");
     }
     rect(SCISSORS_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT);
-}
-
+}}
 function mouseClicked() {
- if (computer === "") {
-        // Check if the click is inside the area of the icons to select one.
-        const isInVertical = mouseY >= ICON_TOP && mouseY < ICON_TOP + ICON_HEIGHT;
-        if (isInVertical && mouseX >= STONE_LEFT && mouseX < STONE_LEFT + ICON_WIDTH) {
-            selected = "🪨"; 
-        }
-
-        if (isInVertical && mouseX >= PAPER_LEFT && mouseX < PAPER_LEFT + ICON_WIDTH) {
-            selected = "📃"; 
-        }
-
-        if (isInVertical && mouseX >= SCISSORS_LEFT && mouseX < SCISSORS_LEFT + ICON_WIDTH) {
-            selected = "✂️"; 
-        }
-
-        const computerSymbolId = Math.floor(random(0, 3));
-        if (computerSymbolId === 0) {
-            computer = "🪨"; 
-        } else if (computerSymbolId === 1) {
-            computer = "📃"; 
-        } else if (computerSymbolId === 2) {
-            computer = "✂️"; 
-        }
-
-        noStroke();
-        fill("yellow");
-        textSize(30);
-        text("Computer:", TEXT_LEFT, 300);
-        textSize(75);
-        text(computer, 175, 300);
-
-        
-        let winner: string = "";
-        if (selected === computer) {
-            winner = "It's a tie!"; 
-        } else if (
-            (selected === "🪨" && computer === "✂️") || 
-            (selected === "📃" && computer === "🪨") ||
-            (selected === "✂️" && computer === "📃")   
-        ) {
-            winner = "You win!"; 
-        } else {
-            winner = "Computer wins!"; 
-        }
-
-        textSize(50);
-        fill("yellow");
-        text(winner, TEXT_LEFT, 450);
+    textSize(50)
+    fill("black")
+    noStroke()
+    rect(0,height/2-20,width,height)
+    let selected: number = 0
+    let output: string = ""
+if(computerv2 === 0){
+    let computer: number = Math.floor(random(1,4))
+    if(computer === 1){ 
+    text("🪨", PAPER_LEFT,300 ); }
+    if(computer === 2){
+        text("📃", PAPER_LEFT, 300);
     }
+    if(computer === 3){
+        text("✂️", PAPER_LEFT, 300);
+    }
+    if (mouseY >= ICON_TOP && mouseY < ICON_TOP + ICON_HEIGHT && mouseX >= STONE_LEFT && mouseX < STONE_LEFT + ICON_WIDTH) {
+        selected = 1}
+    if (mouseY >= ICON_TOP && mouseY < ICON_TOP + ICON_HEIGHT && mouseX >= PAPER_LEFT && mouseX < PAPER_LEFT + ICON_WIDTH) {
+        selected = 2}
+    if (mouseY >= ICON_TOP && mouseY < ICON_TOP + ICON_HEIGHT && mouseX >= SCISSORS_LEFT && mouseX < SCISSORS_LEFT + ICON_WIDTH){
+            selected =3
+        }
+    if( computer === 1 && selected === 1 
+        computer === 2 && selected === 2
+        computer === 3 && selected === 3  ){
+        output= "It's a tie"
 }
+    else if( computer === 1 && selected === 3 
+             computer === 2 && selected === 1 
+             computer === 3 && selected === 2 ){
+                output= "you lose "}
+       else if( computer === 3 && selected === 1 
+                computer === 1 && selected === 2 
+                computer === 2 && selected === 3 ){
+        output = "you win "
+    }
+    fill("yellow")
+    textSize(50)
+    text(${output},20, 350 )
+             }
+             computerv2 = 5
+             stroke("black");
+             noFill()
+             rect(SCISSORS_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT)
+             rect(PAPER_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT)
+             rect(STONE_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT)}
