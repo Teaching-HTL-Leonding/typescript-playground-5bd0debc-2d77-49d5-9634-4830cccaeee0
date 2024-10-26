0ab7e02b0f5e21490d66ecdf14c048e017d39c43
Rainer Stropek <rainer@timecockpit.com>
2024-10-26 18:59:07 +0200

    Saved in TypeScript Playground at 2024-10-26T16:59:07.089Z

--- bouncing_ball_advanced/index.ts
+++ bouncing_ball_advanced/index.ts
@@ -4,6 +4,8 @@ let by: number = 0;
 let dx: number = 1;
 let dy: number = 1;
 
+const rad: number = 50;
+
 function setup() {
   createCanvas(300, 200);
   background("green");
@@ -15,22 +17,26 @@ function draw() {
   background("green");
   fill("yellow");
   noStroke();
-  circle(bx, by, 50);
+  rect(bx, by, rad, rad);
 
   bx = bx + dx;
   by = by + dy;
 
-  if (by > height) {
+  if (by > height - rad) {
     dy = -1;
   }
   else if (by < 0) {
     dy = + 1;
   }
-  else if(bx > width){
+  else if(bx > width - rad){
     dx = -1;
   }
   else if(bx <0){
     dx = +1
   }
+}
+
+function mouseClicked(){
+
 
 }
