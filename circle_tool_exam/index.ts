d192e0394e64786434150fe7686e732b8aff02a6
Rainer Stropek <rainer@timecockpit.com>
2024-10-26 13:12:41 +0200

    Saved in TypeScript Playground at 2024-10-26T11:12:40.581Z

---
+++ circle_tool_exam/index.ts
@@ -0,0 +1,29 @@
+let mx = 0;
+let my = 0;
+
+
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    angleMode(DEGREES);
+
+}
+
+function draw(){
+    
+    noFill();
+    stroke("white");
+    background("black");
+    ellipse(width / 2, height / 2, width - mx, height - my);
+
+}
+
+function mouseMoved(){
+    
+    mx = mouseX * 2;
+    my = mouseY * 2;
+
+
+}
+
+
