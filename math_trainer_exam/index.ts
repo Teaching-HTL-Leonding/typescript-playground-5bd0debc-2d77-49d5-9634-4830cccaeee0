eeaca976766eb56a5e4c6b342ef97718326110f7
Rainer Stropek <rainer@timecockpit.com>
2024-11-07 09:25:58 +0100

    Saved in TypeScript Playground at 2024-11-07T08:25:57.678Z

--- math_trainer_exam/index.ts
+++ math_trainer_exam/index.ts
@@ -7,12 +7,10 @@ let randnum1 = 0;
 let randnum2 = 0;
 // nummern unten
 let randnum3 = 0;
-let randnum4 = 0; 
+let randnum4 = 0;
 let randnum5 = 0;
 
-/*let randtext1 = 0;
-let randtext2 = 0;
-let randtext3 = 0;*/
+let correct = 0;
 
 
 // setup function runs once and sets up the quiz elements on the canvas
@@ -29,20 +27,38 @@ function setup() {
     background("black");
 
     const dice = Math.floor(random(1, 4));
-    let message: string; 
-    if (dice === 1) { message = "+"; }
-    else if (dice === 2) { message = "-"; }
-    else if (dice === 3) { message = "*"; }
+    let message: string;
+    if (dice === 1) {
+        message = "+";
+        correct = randnum1 + randnum2;
+    }
+    else if (dice === 2) {
+        message = "-";
+        correct = randnum1 - randnum2;
+    }
+    else if (dice === 3) {
+        message = "*";
+        correct = randnum1 * randnum2;
+    }
+
+    const computerSymbolId = Math.floor(random(0, 3));
+    if (computerSymbolId === 0) {
+        randnum3 = correct;
+    } else if (computerSymbolId === 1) {
+        randnum4 = correct;
+    } else if (computerSymbolId === 2) {
+        randnum5 = correct;
+    }
 
     fill("yellow");
     textSize(50);
     // Text oben
     text(`${randnum1} ${message} ${randnum2}`, width / 2 - 75, 50);
     // Text unten
-    text(`${randnum3}`, width / 6 - 40, height / 2);
-    text(`${randnum4}`, width / 6 * 3 - 25, height / 2);
-    text(`${randnum5}`, width / 6 * 5 - 10, height / 2);
-    
+    text(`${randnum3}`, width / 6 - 40, height / 2 + 15);
+    text(`${randnum4}`, width / 6 * 3 - 25, height / 2 + 15);
+    text(`${randnum5}`, width / 6 * 5 - 10, height / 2 + 15);
+
 }
 
 // mouseClicked function checks if the user clicked on the correct answer
