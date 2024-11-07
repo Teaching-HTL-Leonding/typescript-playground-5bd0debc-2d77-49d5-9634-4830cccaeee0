395fefebc17bd1ff361c3834f8d7792c30757fc7
Rainer Stropek <rainer@timecockpit.com>
2024-11-07 08:51:38 +0100

    Saved in TypeScript Playground at 2024-11-07T07:51:38.607Z

--- math_trainer_exam/index.ts
+++ math_trainer_exam/index.ts
@@ -2,8 +2,13 @@
 // of a randomly generated arithmetic operation.
 
 // <<< TODO: Declare your GLOBAL variables here
+// nummern oben
 let randnum1 = 0;
 let randnum2 = 0;
+// nummern unten
+let randnum3 = 0;
+let randnum4 = 0; 
+let randnum5 = 0;
 
 /*let randtext1 = 0;
 let randtext2 = 0;
@@ -12,30 +17,32 @@ let randtext3 = 0;*/
 
 // setup function runs once and sets up the quiz elements on the canvas
 function setup() {
+    // random nummern oben
     randnum1 = Math.floor(random(1, 101));
     randnum2 = Math.floor(random(1, 101));
-
-    //randtext = Math.random(+, *, -);
+    // random nummern unten
+    randnum3 = Math.floor(random(1, 101));
+    randnum4 = Math.floor(random(1, 101));
+    randnum5 = Math.floor(random(1, 101));
 
     createCanvas(400, 400);
     background("black");
 
-    fill("yellow");
-    textSize(50);
-    text(`${randnum1} - ${randnum2}`, width / 2 - 75, 50);
-
     const dice = Math.floor(random(1, 4));
-
     let message: string; 
     if (dice === 1) { message = "+"; }
     else if (dice === 2) { message = "-"; }
     else if (dice === 3) { message = "*"; }
 
-    }
-
-
-
-    // <<< TODO: Add your code here
+    fill("yellow");
+    textSize(50);
+    // Text oben
+    text(`${randnum1} ${message} ${randnum2}`, width / 2 - 75, 50);
+    // Text unten
+    text(`${randnum3}`, width / 6 - 40, height / 2);
+    text(`${randnum4}`, width / 6 * 3 - 25, height / 2);
+    text(`${randnum5}`, width / 6 * 5 - 10, height / 2);
+    
 }
 
 // mouseClicked function checks if the user clicked on the correct answer
