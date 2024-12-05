45e7a96fd180213039fa90fad70daa3c123c8084
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 09:49:25 +0100

    Saved in TypeScript Playground at 2024-12-05T08:49:24.840Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -22,7 +22,7 @@ function mouseMoved() {
     // <<< Add your code here
 
     for (let i = 0; i <= height; i++) {
-        stroke(i, i + 360, 60 + i);
+        stroke(i, i + 360, 100);
         translate(0, 10);
         line(0, 0, 10, 5);
         line(10, 5, 0, 10);
