dde0e87612557fee249497a73949bf756c0bc8ee
Rainer Stropek <rainer@timecockpit.com>
2025-01-30 08:16:39 +0100

    Saved in TypeScript Playground at 2025-01-30T07:16:39.115Z

---
+++ intervals/index.ts
@@ -0,0 +1,24 @@
+let interval: number;
+
+function setup() {
+  createCanvas(300, 300);
+  background("black");
+  strokeWeight(2);
+  stroke("lime");
+  noFill();
+
+  // <<< Add code to setup interval here
+  interval = setInterval(drawCircle, 1000);
+}
+
+function mouseClicked() {
+  clearInterval(interval);
+  console.log("Interval cleared");
+}
+
+function drawCircle() {
+  const center_x = random(width);
+  const center_y = random(height);
+  const diameter = random(20, 80);
+  circle(center_x, center_y, diameter)
+}
